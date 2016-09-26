e9868ffc722060110c14ee8ca7e31c84368989c3
cloidnerux <cloidnerux@users.noreply.github.com>
2016-09-26 19:55:11 +0200

    Add files via upload
    Initial commit

---
+++ ImageConverter/ImageConverter/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ImageConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenImageButton = new System.Windows.Forms.Button();
+            this.OutputTextBox = new System.Windows.Forms.RichTextBox();
+            this.ImageProgressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // OpenImageButton
+            // 
+            this.OpenImageButton.Location = new System.Drawing.Point(13, 13);
+            this.OpenImageButton.Name = "OpenImageButton";
+            this.OpenImageButton.Size = new System.Drawing.Size(150, 45);
+            this.OpenImageButton.TabIndex = 0;
+            this.OpenImageButton.Text = "Open Image";
+            this.OpenImageButton.UseVisualStyleBackColor = true;
+            this.OpenImageButton.Click += new System.EventHandler(this.OpenImageButton_Click);
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(13, 65);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(408, 270);
+            this.OutputTextBox.TabIndex = 1;
+            this.OutputTextBox.Text = "";
+            // 
+            // ImageProgressBar
+            // 
+            this.ImageProgressBar.Location = new System.Drawing.Point(199, 23);
+            this.ImageProgressBar.Name = "ImageProgressBar";
+            this.ImageProgressBar.Size = new System.Drawing.Size(222, 23);
+            this.ImageProgressBar.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 347);
+            this.Controls.Add(this.ImageProgressBar);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.OpenImageButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenImageButton;
+        private System.Windows.Forms.RichTextBox OutputTextBox;
+        private System.Windows.Forms.ProgressBar ImageProgressBar;
+    }
+}
+
---
+++ ImageConverter/ImageConverter/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenImageButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+            Bitmap b = new Bitmap(ofd.FileName);
+            OutputTextBox.Text = string.Empty;
+            string result = string.Empty;
+            result += "const unsigned char PROGMEM logo[] = {\n";
+            byte tmp = 0;
+            for (int w = 0; w < b.Width; w++)
+            {
+                tmp = 0;
+                ImageProgressBar.Value = w / b.Width * 100;
+                for (int h = 0; h < b.Height; h+=8)
+                {
+                    for (int i = 0; i < 8; i++)
+                    {
+                        tmp |= (b.GetPixel(w, h + i).GetBrightness() < 0.5) ? ((byte)1) : ((byte)0);
+                        tmp <<= 1;
+                    }
+                    result += "0x" + tmp.ToString("X2") + ", ";
+                }
+                result += "\n";
+            }
+            result += "};";
+            OutputTextBox.Text = result;
+        }
+    }
+}
